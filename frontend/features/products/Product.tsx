6ff2dd7f22237e00273eaec9044995f43c700c09
Sophia Brandt <git@s-br.net>
2021-03-16 07:42:33 +0100

    Add bolder product title, use same font-family for price tag

--- frontend/features/products/Product.tsx
+++ frontend/features/products/Product.tsx
@@ -1,14 +1,15 @@
 import { Maybe, Product as ProductType } from "@/graphql/sdk";
 import { theme } from "@/infrastructure/theme";
 import { formatMoney } from "@/lib/index";
-import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Link, Text } from "@chakra-ui/react";
+import NextLink from "next/link";
 import styled from "@emotion/styled";
 
 interface StyledTextProps {
   theme?: typeof theme;
 }
 
-const PriceTag = styled(Text)<StyledTextProps>`
+const PriceTag = styled(Heading)<StyledTextProps>`
   background: ${(props) => props.theme.colors.brand.primary};
   transform: rotate(3deg);
   color: ${(props) => props.theme.colors.text.inverse};
@@ -17,8 +18,23 @@ const PriceTag = styled(Text)<StyledTextProps>`
   line-height: 1;
   font-size: 2.5rem;
   display: inline-block;
-  top: -3px;
-  right: -3px;
+`;
+
+const Title = styled(Heading)<StyledTextProps>`
+  margin: 0 1rem;
+  text-align: center;
+  transform: skew(-5deg) rotate(-1deg);
+  margin-top: -3rem;
+  text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.1);
+  a {
+    background: ${(props) => props.theme.colors.brand.primary};
+    display: inline;
+    line-height: 1.3;
+    font-size: 2.5rem;
+    text-align: center;
+    color: white;
+    padding: 0 1rem;
+  }
 `;
 
 interface ProductProps {
@@ -39,9 +55,11 @@ export const Product = ({ product }: ProductProps) => {
           alt={product?.photo?.altText as string | undefined}
           fallbackSrc="https://via.placeholder.com/400"
         />
-        <Heading p="1" mt="1" fontWeight="semibold" size="md" as="h3" isTruncated>
-          {product?.name}
-        </Heading>
+        <Title>
+          <NextLink href={`/product/${product?.id}`}>
+            <Link>{product?.name}</Link>
+          </NextLink>
+        </Title>
         <Text p="1" isTruncated>
           {product?.description}
         </Text>
