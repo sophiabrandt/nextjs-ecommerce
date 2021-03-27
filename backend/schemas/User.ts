6a413734cd41d4d59aebd4b0789b463091e807ec
Sophia Brandt <git@s-br.net>
2021-03-27 16:06:51 +0100

    Add user permission

--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -1,7 +1,21 @@
 import { relationship, password, text } from "@keystone-next/fields";
 import { list } from "@keystone-next/keystone/schema";
+import { permissions, rules } from "../access";
 
 export const User = list({
+  access: {
+    create: () => true,
+    read: rules.canManageUsers,
+    update: rules.canManageUsers,
+    // only people with the permission can delete themselves!
+    // You can't delete yourself
+    delete: permissions.canManageUsers,
+  },
+  ui: {
+    // hide the backend UI from regular users
+    hideCreate: (args) => !permissions.canManageUsers(args),
+    hideDelete: (args) => !permissions.canManageUsers(args),
+  },
   fields: {
     name: text({ isRequired: true }),
     email: text({ isRequired: true, isUnique: true }),
